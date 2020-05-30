1cdb70a00b08ddbb632be0dff6a24c1a8d76feb8
João Celso A. Marques <joaocelso01@gmail.com>
2020-05-30 00:01:10 -0300

    Resolução da lista de  exercícios

---
+++ Lista4/ex1.c
@@ -0,0 +1,11 @@
+/*Elabora uma função que permita somar os elementos de um vetor de inteiros.*/
+#include <stdio.h>
+
+int main(){
+    int vetor[5], i, S = 0;
+    for(i=0; i<5; i++){
+        vetor[i] = 5 + i;
+        S = vetor[i] + S;
+    }
+    printf("A soma dos elementos do vetor é %d\n", S);
+}
