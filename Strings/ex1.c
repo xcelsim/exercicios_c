97e873c73c55bad8610730ed12a7b010c3110bf9
João Celso A. Marques <joaocelso01@gmail.com>
2020-05-30 16:33:46 -0300

    Exercícios de String

--- Strings/ex1.c
+++ Strings/ex1.c
@@ -4,11 +4,12 @@ letras tem esse nome.*/
 #include <string.h>
 
 int main(){
+    int i, s = 0;
     char nome[30];
-
-    printf("Digite seu nome:\n");
+    printf("Digite um nome: ");
     scanf("%[^\n]", nome);
-    int s = strlen(nome);
-    printf("O número de caracteres do nome é %d\n", s);
-
+    for (i = 0; i < strlen(nome); i++)
+        if(nome[i] >= 'a' && nome[i] <= 'z' || nome[i] >= 'A' && nome[i] <= 'Z')
+            s++;    
+    printf("O número de letras do nome é %d\n", s);
 }
